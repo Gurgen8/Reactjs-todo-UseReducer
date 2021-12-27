afe09f3acfff46992dd329b1c713937fffb62272
GURGEN <84467354+Gurgen8@users.noreply.github.com>
2021-12-27 19:52:03 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import InputText from './InputText';
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+ <InputText/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
